98d8bbaead7c41f4685a9b942bb86007b0749630
felt0134 <felton12392@gmail.com>
2025-02-18 12:30:52 -0700

    Updates to the test

---
+++ create_tutorial.R
@@ -0,0 +1,13 @@
+#setup tutorials
+
+# To create a new tutorial, use:
+  
+usethis::use_tutorial(
+    "Week_6_Exam_2", 
+    "Week_6_Exam_2", 
+    open = interactive())
+
+#if setup as an answer key, use tutorialize to turn into tutorial:
+tutorialize::tutorialize_setup("bug_tester")
+tutorialize::tutorialize()
+
